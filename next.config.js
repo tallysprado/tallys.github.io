3cebd4a2bb58d9033ac2067e51ee1352e54394af
Tallys <tallys.prado@gmail.com>
2021-03-17 15:51:00 -0300

    add master branch

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd?'https://cdn.statically.io/gh/tallysprado/tallysprado.github.io/gh-pages/':''
+    assetPrefix: isProd?'https://cdn.statically.io/gh/tallysprado/tallysprado.github.io/master/':''
 }
